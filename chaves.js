f0794ff3886fe8b8b03185369fe57976fe3c72c4
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-09-19 11:12:57 -0300

    Add files via upload

---
+++ chaves.js
@@ -0,0 +1,20 @@
+const cliente = {
+    nome:"João"
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+};
+cliente.endereco = {
+   {
+  rua:"R. Josheph Cliember"
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+},
+};
+const chavesDoObjeto = Object.keys(cliente);
+console.log(chavesDoObjeto);
+
+if (!chavesDoObjeto.includes("enderecos")) {
+    console.error("Erro. É necessário ter um endereço cadastrado.");
+}
