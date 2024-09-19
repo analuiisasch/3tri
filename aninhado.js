f1797761d88b7cdc6ac2d5a28c192635bb6f76b2
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-09-19 11:17:37 -0300

    Update aninhado.js

--- aninhado.js
+++ aninhado.js
@@ -1,11 +1,11 @@
 const cliente = {
-    nome:"João"
+    nome:"João",
     idade: 24,
     email: "joao@firma.com",
     telefone: ["1155555550", "1144444440"],
 };
 cliente.endereco = {
-    rua:"R. Josheph Cliember"
+    rua:"R. Josheph Cliember",
     numero: 1337,
     apartamento: true,
     complemento: "ap 934",
