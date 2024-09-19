f002940d9736500a8923810b028d094ff627b8bb
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-09-19 11:52:50 -0300

    Add files via upload

---
+++ espalhamento.js
@@ -0,0 +1,23 @@
+const cliente = {
+    nome:"João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+};
+
+cliente.enderecos= [
+   {
+    rua:"R. Josheph Cliember",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+ },
+
+];
+
+function LigaParaCliente(telefoneComercial, telefoneResidencial) {
+ console.log(`Ligando para ${telefoneComercial}`);
+ console.log(`Ligando para ${telefoneResidencial}`);
+}
+
+ ligaParaCliente(cliente.telefone[0], cliente.telefone[1]);
