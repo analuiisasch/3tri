c5167db52f47b4ab78fa47b2f2ea63ea1dacb1f8
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-09-19 11:06:06 -0300

    Add files via upload

---
+++ aninhado.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome:"João"
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+};
+cliente.endereco = {
+    rua:"R. Josheph Cliember"
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+};
+console.log(cliente)
+console.log(cliente.endereco)
