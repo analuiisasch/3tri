bc5ea56a8dcde81781f6fe590a23962c15a82518
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-09-19 11:19:00 -0300

    Update chaves.js

--- chaves.js
+++ chaves.js
@@ -1,20 +1,22 @@
 const cliente = {
-    nome:"João"
+    nome:"João",
     idade: 24,
     email: "joao@firma.com",
     telefone: ["1155555550", "1144444440"],
 };
-cliente.endereco = {
+cliente.endereco = [
    {
-  rua:"R. Josheph Cliember"
+  rua:"R. Josheph Cliember",
     numero: 1337,
     apartamento: true,
     complemento: "ap 934",
-},
-};
+ },
+
+];
 const chavesDoObjeto = Object.keys(cliente);
 console.log(chavesDoObjeto);
 
 if (!chavesDoObjeto.includes("enderecos")) {
     console.error("Erro. É necessário ter um endereço cadastrado.");
 }
+
