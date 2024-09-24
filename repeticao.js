c74453493580d6370d3d103cbff427c5f5d7ed60
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-09-24 07:56:57 -0300

    Add files via upload

---
+++ repeticao.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome: "João",
+    idade:24,,
+    email: "joao@firma.com"
+    telefone: ["1155555550", "1144444440"]
+};
+cliente.enderecos = [
+    {
+        rua: "R. Joseph Climber",
+        numero: 1337.
+        apartamento: true,
+        complemento: "app 934",
+
+
+    },
+];
+ 
